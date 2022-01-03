9090219921237e4ea11a0a9de5068a64a0fbbf95
amansharma8194 <amankumarsharma8194031598@gmail.com>
2022-01-03 11:52:24 +0530

    initialize firebase for JoSip

---
+++ src/misc/Firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCqoT_HCV2Im4JzkO83nopAbJtBQYUCLrI',
+  authDomain: 'josip-b69a1.firebaseapp.com',
+  databaseURL:
+    'https://josip-b69a1-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'josip-b69a1',
+  storageBucket: 'josip-b69a1.appspot.com',
+  messagingSenderId: '851186823923',
+  appId: '1:851186823923:web:a011151c3edda178493d86',
+};
+
+const app = firebase.initializeApp(config);
