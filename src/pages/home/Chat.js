6b84cdb3475547fd7bcd7b6607d2f35b7d29bf39
amansharma8194 <amankumarsharma8194031598@gmail.com>
2022-01-16 14:25:45 +0530

    created layout for home page and chat page

---
+++ src/pages/home/Chat.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const chat = () => {
+  return <div>chat page</div>;
+};
+
+export default chat;
---
+++ src/pages/home/Homeindex.js
@@ -0,0 +1,43 @@
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router';
+import { Col, Grid, Row } from 'rsuite';
+import Sidebar from '../../components/Sidebar';
+import { RoomsProvider } from '../../context/rooms.context';
+import { useMediaQuery } from '../../misc/custom-hooks';
+import Chat from './Chat';
+
+const Home = () => {
+  const isDesktop = useMediaQuery('(min-width: 992px)');
+  const { isExact } = useRouteMatch();
+  const canRenderSidebar = isDesktop || isExact;
+  return (
+    <RoomsProvider>
+      <Grid fluid className="h-100">
+        <Row className="h-100">
+          {canRenderSidebar && (
+            <Col xs={24} md={6} className="h-100">
+              <Sidebar />
+            </Col>
+          )}
+          <Switch>
+            <Route exact path="/chat/:chatId">
+              <Col>
+                <Chat xs={24} md={18} className="h-100" />
+              </Col>
+            </Route>
+            <Route>
+              {isDesktop && (
+                <Col xs={24} md={18} className=" h-100">
+                  <h6 className="text-center mt-page">Please Select a room.</h6>
+                </Col>
+              )}
+            </Route>
+          </Switch>
+        </Row>
+      </Grid>
+    </RoomsProvider>
+  );
+};
+
+export default Home;
