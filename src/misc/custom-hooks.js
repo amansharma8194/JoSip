1425f466dac813e31daacb931b59f26bac61241b
amansharma8194 <amankumarsharma8194031598@gmail.com>
2022-01-20 20:34:19 +0530

    displayed real time presence

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
+import { database } from './Firebase';
 
 export const useModalToggle = (defaultvalue = false) => {
   const [isOpen, setisOpen] = useState(defaultvalue);
@@ -27,4 +28,20 @@ export const useMediaQuery = query => {
   }, [query]);
 
   return matches;
+};
+export const usePresence = uid => {
+  const [presence, setPresence] = useState(null);
+  useEffect(() => {
+    const userStatusRef = database.ref(`status/${uid}`);
+    userStatusRef.on('value', snap => {
+      if (snap.exists()) {
+        const data = snap.val();
+        setPresence(data);
+      }
+    });
+    return () => {
+      userStatusRef.off();
+    };
+  }, [uid]);
+  return presence;
 };
